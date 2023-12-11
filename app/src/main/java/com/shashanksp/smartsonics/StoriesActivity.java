e582c09c69e4a187d7bdc1cb90f5ae12abf0e5d4
shashank <shashankprince523@gmail.com>
2023-12-11 11:32:10 +0530

    ui of stories activity done

--- app/src/main/java/com/shashanksp/smartsonics/StoriesActivity.java
+++ app/src/main/java/com/shashanksp/smartsonics/StoriesActivity.java
@@ -3,7 +3,6 @@ package com.shashanksp.smartsonics;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
