b925eea07a64b56870a1880d54c2ce07d2611715
shashank <shashankprince523@gmail.com>
2023-12-18 19:09:47 +0530

    username is visible in story and used all over except in the guide list

--- app/src/main/java/com/shashanksp/smartsonics/SplashActivity.java
+++ app/src/main/java/com/shashanksp/smartsonics/SplashActivity.java
@@ -4,25 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
     private static final String PREF_GUIDE_ID = "guideId";
     private static final String PREF_IS_GUIDE = "isGuide";
-    private static final String USER_EMAIL = "anonymous";
-    private static final String USERNAME = " ";
-    private TextView welcomeTxt;
-    private String username;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        username = getUsernameFromPrefs();
-        welcomeTxt = findViewById(R.id.welcomeTxt);
-        welcomeTxt.setText("Welcome "+ username);
 
         Thread timerThread = new Thread() {
             public void run() {
@@ -50,10 +42,6 @@ public class SplashActivity extends AppCompatActivity {
     private boolean getIsGuideFromPrefs() {
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean(PREF_IS_GUIDE, false);
-    }
-    private String getUsernameFromPrefs() {
-        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        return sharedPreferences.getString(USERNAME,"anonymous user");
     }
     private void clearSharedPreferences() {
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
